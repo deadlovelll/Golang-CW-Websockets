86660e15519df9763430b6de54ea25e3697fc026
deadlovelll <playboicartina@gmail.com>
2025-02-28 02:02:51 +0300

    removed duplicated file

--- search/hashtags_search/modules/base_controller/base_controller 2.go
+++
@@ -1,9 +0,0 @@
-package basecontroller
-
-import (
-	"hashtags_search/modules/database"
-)
-
-type BaseController struct {
-	Database *database.Database
-}
--- search/places_search/modules/base_controller/base_controller 2.go
+++
@@ -1,9 +0,0 @@
-package basecontroller
-
-import (
-	"places_search/modules/database"
-)
-
-type BaseController struct {
-	Database *database.Database
-}

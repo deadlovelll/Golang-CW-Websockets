18afe230cf43657d0bf340ea9001913ea79646b1
deadlovelll <playboicartina@gmail.com>
2025-03-01 11:36:55 +0300

    improved base controller

--- messenger_engine/controllers/base_controller/base_controller.go
+++ messenger_engine/controllers/base_controller/base_controller.go
@@ -1,9 +1,25 @@
 package basecontroller
 
 import (
+	"log"
+
 	"messenger_engine/modules/database"
 )
 
+// BaseController provides a common structure for controllers with database access.
 type BaseController struct {
-	Database *database.Database
+	db *database.Database
+}
+
+// NewBaseController initializes a new BaseController.
+func NewBaseController(db *database.Database) *BaseController {
+	if db == nil {
+		log.Fatal("BaseController: database instance cannot be nil")
+	}
+	return &BaseController{db: db}
+}
+
+// GetDatabase returns the database instance.
+func (bc *BaseController) GetDatabase() *database.Database {
+	return bc.db
 }
